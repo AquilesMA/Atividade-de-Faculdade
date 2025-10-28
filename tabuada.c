1b35b12d39dafe776a0a0d4a82bf657a99a937fc
Aquiles Maciel <aquilesmaciel16@gmail.com>
2025-10-28 11:17:21 -0300

    Primeiro Commit

---
+++ tabuada.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    printf("TABUADA\n");  
+    
+    for (int i = 0; i < 11; i++) {
+        printf("5\n",5*i);  // %d indica número inteiro, \n pula linha
+    }
+
+    return 0;
+}
